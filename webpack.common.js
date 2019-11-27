e722293183b87f9a11bbb364047f7bb81d6ce8f6
JozsefVass <joco.udv@gmail.com>
2019-11-27 19:23:26 +0000

    Dotenv fixed

--- webpack.common.js
+++ webpack.common.js
@@ -2,7 +2,7 @@ const path = require('path');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyPlugin = require('copy-webpack-plugin');
-const Dotenv = require('dotenv-webpack');
+const Dotenv = require('dotenv');
 
 module.exports = {
   entry: ['@babel/polyfill', './src/js/app.js'],
