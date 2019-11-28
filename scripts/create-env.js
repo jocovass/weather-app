000323461cb6c24da1c6e384eaff6c5cdef2f3fc
JozsefVass <joco.udv@gmail.com>
2019-11-28 16:57:10 +0000

    testing

--- scripts/create-env.js
+++ scripts/create-env.js
@@ -1,4 +1,4 @@
 const fs = require('fs');
 
-fs.writeFileSync('./.env', [`APIKEY=${process.env.APIKEY}\n`,
-                           `PROXY=${process.env.PROXY}\n`]);
+fs.writeFileSync('./.env', `PROXY=${process.env.PROXY}\n`);
+fs.writeFileSync('./.env', `APIKEY=${process.env.APIKEY}\n`);
